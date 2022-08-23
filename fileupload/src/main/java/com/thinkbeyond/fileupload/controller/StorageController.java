be8b6239835a71f41303683266ca357ec424d456
B Vamshi Reddy <64647063+vamshi-breddy@users.noreply.github.com>
2022-08-23 20:50:25 +0530

    Add files via upload
    This project shows how to upload and download a image using spring boot

---
+++ fileupload/src/main/java/com/thinkbeyond/fileupload/controller/StorageController.java
@@ -0,0 +1,35 @@
+package com.thinkbeyond.fileupload.controller;
+
+import com.thinkbeyond.fileupload.service.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/image")
+public class StorageController {
+
+    @Autowired
+    private StorageService service;
+
+    @PostMapping
+    public ResponseEntity<?> uploadImage(@RequestParam("image") MultipartFile file) throws IOException {
+        String uploadImage = service.uploadImage(file);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(uploadImage);
+    }
+
+    @GetMapping("/{fileName}")
+    public ResponseEntity<?> downloadImage(@PathVariable String fileName){
+        byte[] imageData=service.downloadImage(fileName);
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.valueOf("image/png"))
+                .body(imageData);
+
+    }
+}
---
+++ fileupload/src/main/java/com/thinkbeyond/fileupload/repository/StorageRepository.java
@@ -0,0 +1,11 @@
+package com.thinkbeyond.fileupload.repository;
+
+import com.thinkbeyond.fileupload.entity.ImageData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface StorageRepository extends JpaRepository<ImageData,Long> {
+
+    Optional<ImageData> findByName(String fileName);
+}
